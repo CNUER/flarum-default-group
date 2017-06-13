1fa2604aa8eceed8f8c693ce6eb0f4fcff0ab46c
cc <cc@cnuer.cn>
2017-06-13 23:38:50 +0800

    change group base on email

--- js/admin/src/components/DefaultGroupSettingsModal.js
+++ js/admin/src/components/DefaultGroupSettingsModal.js
@@ -13,9 +13,12 @@ export default class DefaultGroupSettingsModal extends SettingsModal {
 
     form() {
         this.defaultGroup = this.setting('hyn.default_group.group') || Group.MEMBER_ID;
+        this.cnuGroup = this.setting('hyn.cnu_group.group') || Group.MEMBER_ID;
+        this.cnuGroupRegx = this.setting('hyn.cnuGroup_regx') ;
         this.groups = app.store.all('groups')
             .filter(group => [Group.GUEST_ID].indexOf(group.id()) === -1);
         return [
+         <div className="Form">
             <div className="Form-group">
                 <label>Default group</label>
                 <select name="defaultGroup" bidi={this.defaultGroup}>
@@ -26,6 +29,23 @@ export default class DefaultGroupSettingsModal extends SettingsModal {
                     )}
                 </select>
             </div>
+            
+            <div className="Form-group">
+                <label>cnu group</label>
+                <select name="cnuGroup" bidi={this.cnuGroup}>
+                    {this.groups.map(group =>
+                    <option value={group.id()}>
+                        {group.namePlural()}
+                    </option>
+                    )}
+                </select>
+            </div>
+            
+            <div className="Form-group">
+                <label>email regx</label>
+                <input className="FormControl" bidi={this.cnuGroupRegx}/>
+            </div>
+        </div>
         ];
     }
 }
--- src/Listeners/AddDefaultGroup.php
+++ src/Listeners/AddDefaultGroup.php
@@ -1,6 +1,8 @@
-<?php namespace Hyn\DefaultGroup\Listeners;
+<?php 
+namespace Hyn\DefaultGroup\Listeners;
 
 use Flarum\Event\UserWasActivated;
+use Flarum\Event\UserEmailWasChanged;
 use Flarum\Core\Group;
 
 use Flarum\Settings\SettingsRepositoryInterface;
@@ -17,11 +19,19 @@ class AddDefaultGroup
      * @var int
      */
     protected $defaultGroup;
+    protected $cnuGroup;
+    
+    /**
+     * @var string
+     */
+    protected $cnuGroup_regx;
 
     public function __construct(SettingsRepositoryInterface $settings) {
         $this->settings = $settings;
 
         $this->defaultGroup = (int) $this->settings->get('hyn.default_group.group', Group::MEMBER_ID);
+        $this->cnuGroup = (int) $this->settings->get('hyn.cnu_group.group', Group::MEMBER_ID);
+        $this->cnuGroup = $this->settings->get('hyn.cnuGroup_regx', '@cnu.edu.cn$');
     }
 
     /**
@@ -31,6 +41,7 @@ class AddDefaultGroup
     public function subscribe(Dispatcher $events)
     {
         $events->listen(UserWasActivated::class, [$this, 'addGroup']);
+        $events->listen(UserEmailWasChanged::class, [$this, 'changeGroup']);
     }
 
     /**
@@ -38,9 +49,27 @@ class AddDefaultGroup
      * @param UserWasActivated $event
      */
     public function addGroup(UserWasActivated $event) {
+        //认证用户组
+        if($this->cnuGroup && preg_match('/'.$this->cnuGroup_regx.'/',$event->user->email)) {
+            $event->user->groups()->attach($this->cnuGroup);
+            return;
+        }
+
         if($this->defaultGroup == Group::MEMBER_ID) {
             return;
         }
         $event->user->groups()->attach($this->defaultGroup);
+    }
+
+    /**
+     * Attaches the default group to the activated user
+     * @param UserWasActivated $event
+     */
+    public function changeGroup(UserEmailWasChanged $event) {
+        if($this->cnuGroup && preg_match('/'.$this->cnuGroup_regx.'/',$event->user->email)) {
+            $event->user->groups()->attach($this->cnuGroup);
+        }elseif($event->user->groups()->detach($this->cnuGroup)){//成功踢出认证组
+            $event->user->groups()->attach($this->defaultGroup);//加入普通组
+        }
     }
 }
